fdd2fea4944ab89a333176ddb517bcb773fe5c73
AritoMelo <aritomelo@gmail.com>
2018-04-23 16:11:51 +0200

    OPENEUROPA-336: Custom event, Services, Entity.  - Created a custom event listener "WebtoolsImportSettingsEvent"
     - An Entity class called WebtoolsAnalyticsSearch to represent search field on event listener.
    
     - Custom event subscriber to listen to the WebtoolsImportSettings event.

---
+++ oe_webtools_analytics/src/Entity/WebtoolsAnalyticsSearch.php
@@ -0,0 +1,99 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * Contains the class that will represent the search field on Event listener.
+ *
+ * @see https://webgate.ec.europa.eu/fpfis/wikis/pages/viewpage.action?spaceKey=webtools&title=Piwik
+ */
+
+namespace Drupal\oe_webtools_analytics\Entity;
+
+use JsonSerializable;
+
+/**
+ * Class WebtoolsAnalyticsSearch.
+ *
+ * @package Drupal\oe_webtools_analytics\Entity
+ */
+class WebtoolsAnalyticsSearch implements jsonserializable {
+  /**
+   * Keyword searched (mandatory).
+   *
+   * @var string
+   */
+  protected $keyword;
+
+  /**
+   * Category of the search (optional).
+   *
+   * @var string
+   */
+  protected $category;
+
+  /**
+   * Count of search results (optional).
+   *
+   * @var int
+   *   An integer indicating how many results were found.
+   */
+  protected $count;
+
+  /**
+   * Sets the search keyword.
+   *
+   * @param string $keyword
+   *   A mandatory string value.
+   */
+  public function setKeyword(string $keyword): void {
+    $this->keyword = $keyword;
+  }
+
+  /**
+   * Sets search category.
+   *
+   * @param string $category
+   *   An optional string value.
+   */
+  public function setCategory($category): void {
+    $this->category = $category;
+  }
+
+  /**
+   * Sets search results count.
+   *
+   * @param int $count
+   *   An optional number value.
+   */
+  public function setCount(int $count): void {
+    $this->count = $count;
+  }
+
+  /**
+   * Check whether or not the keyword has been set.
+   *
+   * @return bool
+   *   True in case the variable has been set otherwise false.
+   */
+  public function isSetKeyword() {
+    return !empty($this->keyword);
+  }
+
+  /**
+   * Serialize the data with custom indexes.
+   *
+   * @return array|mixed
+   *   An array of above parameters.
+   */
+  public function jsonSerialize() {
+    $search = [
+      'keyword' => $this->keyword,
+      'category' => $this->category,
+      'count' => $this->count,
+    ];
+
+    return array_filter($search);
+  }
+
+}
---
+++ oe_webtools_analytics/src/EventSubscriber/WebtoolsImportSettingsSubscriber.php
@@ -0,0 +1,98 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * @file
+ * Listening to the WebtoolsImportSettingsEvent.
+ */
+
+namespace Drupal\oe_webtools_analytics\EventSubscriber;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Url;
+use Drupal\oe_webtools_analytics\Event\WebtoolsImportSettingsEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+/**
+ * Event Subscriber WebtoolsImportSettingsSubscriber.
+ */
+class WebtoolsImportSettingsSubscriber implements EventSubscriberInterface {
+  /**
+   * The Configuration overrides instance.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $config;
+
+  /**
+   * {@inheritdoc}
+   *
+   * @var \Symfony\Component\HttpFoundation\RequestStack
+   */
+  protected $requestStack;
+
+  /**
+   * WebtoolsImportSettingsSubscriber constructor.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $configFactory
+   *   The configuration object.
+   * @param \Symfony\Component\HttpFoundation\RequestStack $requestStack
+   *   The request on the stack.
+   */
+  public function __construct(ConfigFactoryInterface $configFactory, RequestStack $requestStack) {
+    // Get id from settings.php!
+    if ($configFactory->get('oe_webtools.analytics')) {
+      $this->config = $configFactory->get('oe_webtools.analytics');
+      $this->requestStack = $requestStack;
+    }
+    else {
+      throw new \InvalidArgumentException(t('The "oe_webtools.analytics" settings is missing from settings.php', [], ['context' => 'oe_webtools']));
+    }
+  }
+
+  /**
+   * Kernel request event handler.
+   *
+   * @param \Drupal\oe_webtools_analytics\Event\WebtoolsImportSettingsEvent $event
+   *   Response event.
+   */
+  public function onSetSiteDefaults(WebtoolsImportSettingsEvent $event) {
+    // SiteID.
+    $site_id = $this->config->get('site_id');
+    if ($site_id) {
+      $event->setSiteId((string) $site_id);
+    }
+    // SitePath.
+    if (\Drupal::configFactory()->get('oe_webtools.analytics')->get('site_path')) {
+      $event->setSitePath((array) $this->config->get('site_path'));
+    }
+    else {
+      $event->setSitePath((array) ($_SERVER['HTTP_HOST'] . Url::fromRoute('<front>')->toString()));
+    }
+
+    // Set exception flags when access is denied, or page not found.
+    $request_exception = $this->requestStack->getCurrentRequest()->attributes->get('exception');
+    if ($request_exception instanceof NotFoundHttpException) {
+      $event->setIs404Page();
+    }
+
+    if ($request_exception instanceof AccessDeniedHttpException) {
+      $event->setIs403Page();
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    // Subscribing to listening to the WebtoolsImportSettings event.
+    $events[WebtoolsImportSettingsEvent::NAME][] = ['onSetSiteDefaults'];
+
+    return $events;
+  }
+
+}
