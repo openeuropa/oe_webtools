fb4e244b73ddc622cd78d44e42a97da1a3b44362
AritoMelo <aritomelo@gmail.com>
2018-04-26 11:18:44 +0200

    OPENEUROPA-336: Added kernel test.

---
+++ oe_webtools_analytics/tests/src/Kernel/WebtoolsImportSettingsServiceTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drupal\Tests\oe_webtools_analytics\Kernel;
+
+use Drupal\KernelTests\KernelTestBase;
+use Drupal\oe_webtools_analytics\EventSubscriber\WebtoolsImportSettingsSubscriber;
+
+/**
+ * Test to ensure 'WebtoolsImportSettingsSubscriber' service is reachable.
+ *
+ * @package Drupal\Tests\oe_webtools_analytics\Kernel
+ */
+class WebtoolsImportSettingsServiceTest extends KernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = ['oe_webtools_analytics'];
+
+  /**
+   * Test for existence of 'WebtoolsAnalyticsSearch' service.
+   */
+  public function testWebtoolsImportSettingsService() {
+    $subscriber = $this->container->get('oe_webtools_analytics.event_subscriber');
+    $this->assertInstanceOf(WebtoolsImportSettingsSubscriber::class, $subscriber);
+  }
+
+}
