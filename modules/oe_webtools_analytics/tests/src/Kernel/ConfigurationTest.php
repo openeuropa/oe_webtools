933ea63c3487c5b22a38c083113744144cc5c08f
Pol Dellaiera <pol.dellaiera@protonmail.com>
2018-10-18 12:55:14 +0200

    OPENEUROPA-1247: Replace drupal_flush_all_caches() with something else.

--- modules/oe_webtools_analytics/tests/src/Kernel/ConfigurationTest.php
+++ modules/oe_webtools_analytics/tests/src/Kernel/ConfigurationTest.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 
 namespace Drupal\Tests\oe_webtools_analytics\Kernel;
 
+use Drupal\Core\Cache\Cache;
 use Drupal\oe_webtools_analytics\AnalyticsEventInterface;
 use Drupal\Tests\BrowserTestBase;
 
@@ -28,12 +29,17 @@ class ConfigurationTest extends BrowserTestBase {
    * Test that the Webtools JavaScript library is correctly loaded on a page.
    */
   public function testLibraryLoading(): void {
-    \Drupal::configFactory()
+    $config = \Drupal::configFactory()
       ->getEditable(AnalyticsEventInterface::CONFIG_NAME)
       ->set("siteID", "123")
-      ->set("sitePath", "ec.europa.eu")
-      ->save();
-    drupal_flush_all_caches();
+      ->set("sitePath", "ec.europa.eu");
+    $config->save();
+
+    foreach (Cache::getBins() as $service_id => $cache_backend) {
+      if ('dynamic_page_cache' === $service_id || 'page' === $service_id) {
+        $cache_backend->deleteAll();
+      }
+    }
 
     $this->drupalGet('<front>');
     $this->assertSession()
