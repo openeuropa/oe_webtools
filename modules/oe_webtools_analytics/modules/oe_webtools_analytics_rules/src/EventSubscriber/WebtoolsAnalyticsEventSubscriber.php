6b906d5fdd9dfda34782f070d6be8ad047610133
Imanol Eguskiza <ier@opendeusto.es>
2018-11-13 11:29:39 +0100

    OPENEUROPA-665: Inject services instead of sideloading them.

--- modules/oe_webtools_analytics/modules/oe_webtools_analytics_rules/src/EventSubscriber/WebtoolsAnalyticsEventSubscriber.php
+++ modules/oe_webtools_analytics/modules/oe_webtools_analytics_rules/src/EventSubscriber/WebtoolsAnalyticsEventSubscriber.php
@@ -4,14 +4,43 @@ declare(strict_types = 1);
 
 namespace Drupal\oe_webtools_analytics_rules\EventSubscriber;
 
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\oe_webtools_analytics\Event\AnalyticsEvent;
 use Drupal\oe_webtools_analytics\AnalyticsEventInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
  * Event subscriber for the Webtools Analytics event.
  */
 class WebtoolsAnalyticsEventSubscriber implements EventSubscriberInterface {
+
+  /**
+   * The event type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  private $eventTypeManager;
+
+  /**
+   * The request stack.
+   *
+   * @var \Symfony\Component\HttpFoundation\RequestStack
+   */
+  private $requestStack;
+
+  /**
+   * WebtoolsAnalyticsEventSubscriber constructor.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $eventTypeManager
+   *   The event type manager.
+   * @param \Symfony\Component\HttpFoundation\RequestStack $requestStack
+   *   The request stack.
+   */
+  public function __construct(EntityTypeManagerInterface $eventTypeManager, RequestStack $requestStack) {
+    $this->eventTypeManager = $eventTypeManager;
+    $this->requestStack = $requestStack;
+  }
 
   /**
    * Webtools Analytics event handler.
@@ -24,14 +53,14 @@ class WebtoolsAnalyticsEventSubscriber implements EventSubscriberInterface {
    */
   public function setSection(AnalyticsEventInterface $event): void {
     try {
-      $storage = \Drupal::entityTypeManager()
+      $storage = $this->eventTypeManager
         ->getStorage('webtools_analytics_rule');
     }
     catch (\Exception $e) {
       throw new \RuntimeException($e->getMessage());
     }
     $rules = $storage->loadMultiple();
-    $current_uri = \Drupal::request()->getRequestUri();
+    $current_uri = $this->requestStack->getCurrentRequest()->getRequestUri();
     /** @var \Drupal\oe_webtools_analytics_rules\Entity\WebtoolsAnalyticsRuleInterface $rule */
     foreach ($rules as $rule) {
       if (preg_match($rule->getRegex(), $current_uri, $matches) === 1) {
