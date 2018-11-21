730c63e6341169da69237722114b028976c59257
Pol Dellaiera <pol.dellaiera@protonmail.com>
2018-11-21 14:18:14 +0100

    OPENEUROPA-665: Add a regular expression validation in the form only.

--- modules/oe_webtools_analytics/modules/oe_webtools_analytics_rules/src/Form/WebtoolsAnalyticsRuleForm.php
+++ modules/oe_webtools_analytics/modules/oe_webtools_analytics_rules/src/Form/WebtoolsAnalyticsRuleForm.php
@@ -55,6 +55,26 @@ class WebtoolsAnalyticsRuleForm extends EntityForm {
   /**
    * {@inheritdoc}
    */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    parent::validateForm($form, $form_state);
+
+    if (@preg_match($form_state->getValue('regex'), 'openeuropa') === FALSE) {
+      $errors = [
+        PREG_NO_ERROR => 'Code 0 : No errors',
+        PREG_INTERNAL_ERROR => 'Code 1 : There was an internal PCRE error',
+        PREG_BACKTRACK_LIMIT_ERROR => 'Code 2 : Backtrack limit was exhausted',
+        PREG_RECURSION_LIMIT_ERROR => 'Code 3 : Recursion limit was exhausted',
+        PREG_BAD_UTF8_ERROR => 'Code 4 : The offset didn\'t correspond to the begin of a valid UTF-8 code point',
+        PREG_BAD_UTF8_OFFSET_ERROR => 'Code 5 : Malformed UTF-8 data',
+      ];
+
+      $form_state->setErrorByName('regex', $this->t('The regex is not valid. (%error)', ['%error' => $errors[preg_last_error()]]));
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function save(array $form, FormStateInterface $form_state): void {
     /** @var \Drupal\oe_webtools_analytics_rules\Entity\WebtoolsAnalyticsRuleInterface $rule */
     $rule = $this->entity;
