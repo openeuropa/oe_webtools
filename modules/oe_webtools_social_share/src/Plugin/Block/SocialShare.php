e2d87dd76cd1edf9163452c1ff3f30fd1bfd66de
Imanol Eguskiza <ier@opendeusto.es>
2019-03-27 14:14:36 +0100

    OPENEUROPA-1747: Add social sharing block.

---
+++ modules/oe_webtools_social_share/src/Plugin/Block/SocialShare.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\oe_webtools_social_share\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'Social Share' Block.
+ *
+ * @Block(
+ *   id = "social_share",
+ *   admin_label = @Translation("Social Share"),
+ *   category = @Translation("Webtools"),
+ * )
+ */
+class SocialShare extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $social_share_json = [
+      'service' => 'share',
+      'popup' => FALSE,
+      'selection' => TRUE,
+      'to' => [
+        'more',
+        'twitter',
+        'facebook',
+        'linkedin',
+        'e-mail',
+      ],
+      'stats' => TRUE,
+    ];
+    return [
+      'script' => [
+        '#type' => 'html_tag',
+        '#tag' => 'script',
+        '#value' => json_encode($social_share_json),
+        '#attributes' => ['type' => 'application/json'],
+      ],
+    ];
+  }
+
+}
