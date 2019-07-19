300c94418b4b0928dfbd7984cfc00856a414161e
Sergii Pavlenko <sergii.pavlenko.v@gmail.com>
2019-07-19 15:40:19 +0200

    OPENEUROPA-2044: Add behat tests.

---
+++ tests/Behat/WebtoolsGlobanContext.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\oe_webtools\Behat;
+
+use Behat\Behat\Hook\Scope\BeforeScenarioScope;
+use Drupal\DrupalExtension\Context\RawDrupalContext;
+
+/**
+ * Behat step definitions related to the oe_webtools_globan module.
+ */
+class WebtoolsGlobanContext extends RawDrupalContext {
+
+  /**
+   * The config context.
+   *
+   * @var \Drupal\DrupalExtension\Context\ConfigContext
+   */
+  protected $configContext;
+
+  /**
+   * Gathers some other contexts.
+   *
+   * @param \Behat\Behat\Hook\Scope\BeforeScenarioScope $scope
+   *   The before scenario scope.
+   *
+   * @BeforeScenario
+   */
+  public function gatherContexts(BeforeScenarioScope $scope) {
+    $environment = $scope->getEnvironment();
+    $this->configContext = $environment->getContext('Drupal\DrupalExtension\Context\ConfigContext');
+  }
+
+  /**
+   * Backup globan settings config to be restored in an after-scenario.
+   *
+   * @BeforeScenario @backup-globan-settings
+   */
+  public function backupGlobanConfigs(): void {
+    $name = 'oe_webtools_globan.settings';
+    $configs = $this->getDriver()->getCore()->configGet($name);
+    foreach ($configs as $key => $value) {
+      $this->configContext->setConfig($name, $key, $value);
+    }
+  }
+
+  /**
+   * Make sure that load.js with correct parameters.
+   *
+   * @param string $globan_option
+   *   The globan option value.
+   * @param string|null $globan_lang
+   *   The globan language option value.
+   *
+   * @throws \Behat\Mink\Exception\ElementNotFoundException
+   *
+   * @Then Webtools javascript loaded with globan option :globan_option
+   * @Then Webtools javascript loaded with globan option :globan_option and language option :globan_lang
+   */
+  public function assertJsGlobanOption(string $globan_option, $globan_lang = NULL): void {
+    $lang_option = $globan_lang ? '&lang=' . $globan_lang : '';
+    $this->assertSession()->elementExists('css', 'script[src$="load.js?globan=' . $globan_option . $lang_option . '"]');
+  }
+
+}
---
+++ tests/features/global-banner.feature
@@ -0,0 +1,30 @@
+@api
+Feature: Webtools Global Banner
+  In order to provide global banner appearance
+  As privileged user
+  I need to be able to configure the settings of global banner
+
+  @backup-globan-settings
+  Scenario: Privileged user can change Global Banner settings.
+    Given I am logged in as a user with the "administer webtools globan, access administration pages" permission
+    When I am on "the Webtools Globan configuration page"
+    And I select "Yes - display flag" from "Display EU flag"
+    And I select "Dark" from "Background theme"
+    And I select "Yes - show link" from "See all EU Institutions and bodies"
+    And I press "Save"
+    And I am on homepage
+    Then Webtools javascript loaded with globan option "111"
+
+    When I am on "the Webtools Globan configuration page"
+    And I select "No - hide flag" from "Display EU flag"
+    And I select "Light" from "Background theme"
+    And I select "No - hide link" from "See all EU Institutions and bodies"
+    And I press "Save"
+    And I am on homepage
+    Then Webtools javascript loaded with globan option "000"
+
+    When I am on "the Webtools Globan configuration page"
+    When I select "English" from "Override page language"
+    And I press "Save"
+    And I am on homepage
+    Then Webtools javascript loaded with globan option "000" and language option "en"
