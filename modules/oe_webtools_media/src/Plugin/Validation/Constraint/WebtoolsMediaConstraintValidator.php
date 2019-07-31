27d3312fc57903285b4fca05ca6a92ff39337d6a
Mohammed Nassar <Mohammed.NASSAR@ext.ec.europa.eu>
2019-07-31 14:06:23 +0000

    OPENEUROPA-2130: Apply the ValidWebtoolsMedia on media entities only.

--- modules/oe_webtools_media/src/Plugin/Validation/Constraint/WebtoolsMediaConstraintValidator.php
+++ modules/oe_webtools_media/src/Plugin/Validation/Constraint/WebtoolsMediaConstraintValidator.php
@@ -7,6 +7,7 @@ namespace Drupal\oe_webtools_media\Plugin\Validation\Constraint;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 use Drupal\Component\Serialization\Json;
+use Drupal\media\MediaSourceInterface;
 
 /**
  * Validates the webtools media constraint.
@@ -17,8 +18,18 @@ class WebtoolsMediaConstraintValidator extends ConstraintValidator {
    * {@inheritdoc}
    */
   public function validate($value, Constraint $constraint) {
-    $data = Json::decode($value->getValue()[0]['value']);
-    if (!isset($data['service']) || $data['service'] != $constraint->widgetType) {
+    /** @var \Drupal\media\MediaInterface $media */
+    $media = $value->getEntity();
+
+    /** @var \Drupal\media\MediaSourceInterface $source */
+    $source = $media->getSource();
+
+    if (!($source instanceof MediaSourceInterface)) {
+      throw new \LogicException('Media source must implement ' . MediaSourceInterface::class);
+    }
+
+    $snippet = Json::decode($source->getSourceFieldValue($media));
+    if (!isset($snippet['service']) || $snippet['service'] !== $constraint->widgetType) {
       $this->context->addViolation($constraint->message, ['%widgetType' => $constraint->widgetType]);
     }
   }
