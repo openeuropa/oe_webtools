a6a20b2ccc50d29d8865c586beed20f8960089dc
Sergii Pavlenko <sergii.pavlenko.v@gmail.com>
2020-04-20 13:38:30 +0200

    OPENEUROPA-2660: Add upgrade path and hook_install.

---
+++ modules/oe_webtools_media/oe_webtools_media.post_update.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @file
+ * Post update functions for OpenEuropa Webtools Media module.
+ */
+
+declare(strict_types = 1);
+
+use Drupal\Core\File\Exception\FileException;
+use Drupal\Core\File\FileSystemInterface;
+
+/**
+ * Move svg files for webtools media types into the public folder.
+ *
+ * @see \media_install()
+ */
+function oe_webtools_media_post_update_00001(): void {
+  $source = drupal_get_path('module', 'oe_webtools_media') . '/images/icons';
+  $destination = \Drupal::config('media.settings')->get('icon_base_uri');
+  /** @var \Drupal\Core\File\FileSystemInterface $file_system */
+  $file_system = \Drupal::service('file_system');
+  $file_system->prepareDirectory($destination, FileSystemInterface::CREATE_DIRECTORY | FileSystemInterface::MODIFY_PERMISSIONS);
+
+  $files = $file_system->scanDirectory($source, '/.*\.(svg|png|jpg|jpeg|gif)$/');
+  foreach ($files as $file) {
+    if (!file_exists($destination . DIRECTORY_SEPARATOR . $file->filename)) {
+      try {
+        $file_system->copy($file->uri, $destination, FileSystemInterface::EXISTS_ERROR);
+      }
+      catch (FileException $e) {
+        // Ignore and continue.
+      }
+    }
+  }
+}
