4d0eba0c2ffb8f1d130896abef908801ec21d7f8
Pieter Frenssen <pieter@frenssen.be>
2021-06-04 00:58:44 +0300

    ISAICP-6273: Add Behat step definitions for testing the presence of eTrans elements.

---
+++ tests/Behat/WebtoolsETransContext.php
@@ -0,0 +1,94 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\oe_webtools\Behat;
+
+use Drupal\DrupalExtension\Context\RawDrupalContext;
+use Drupal\oe_webtools_etrans\Plugin\Block\ETransBlock;
+
+/**
+ * Behat step definitions for testing Webtools eTrans.
+ *
+ * @SuppressWarnings(PHPMD.CyclomaticComplexity)
+ */
+class WebtoolsETransContext extends RawDrupalContext {
+
+  /**
+   * Checks that an eTrans element is present on the page.
+   *
+   * @param string|null $type
+   *   The type of eTrans element. Can be either 'button', 'icon' or 'link'.
+   *   Omit this parameter to check that any of the possible element types is
+   *   present.
+   *
+   * @throws \RuntimeException
+   *   Thrown when the eTrans element was not found.
+   *
+   * @Then I should see a Webtools eTrans element
+   * @Then I should see the Webtools eTrans :type
+   */
+  public function assertETransPresent(?string $type = ''): void {
+    assert(empty($type) || in_array($type, ETransBlock::RENDER_OPTIONS), 'Element type should be either "button", "icon" or "link."');
+    foreach ($this->getWebtoolsETransElements() as $data) {
+      $types_to_check = $type ? [$type] : ETransBlock::RENDER_OPTIONS;
+      foreach ($types_to_check as $type_to_check) {
+        if ($data->renderAs->$type_to_check ?? FALSE) {
+          // The element is found.
+          return;
+        }
+      }
+    }
+    $type = $type ?: 'element';
+    throw new \RuntimeException("Webtools eTrans $type was not found in the page.");
+  }
+
+  /**
+   * Checks that an eTrans element is not present on the page.
+   *
+   * @param string|null $type
+   *   The type of eTrans element. Can be either 'button', 'icon' or 'link'.
+   *   Omit this parameter to check that none of the possible element types is
+   *   present.
+   *
+   * @throws \RuntimeException
+   *   Thrown when the eTrans element was unexpectedly found.
+   *
+   * @Then I should not see the Webtools eTrans :type
+   * @Then I should not see any Webtools eTrans elements
+   */
+  public function assertNoETransPresent(?string $type = ''): void {
+    assert(empty($type) || in_array($type, ETransBlock::RENDER_OPTIONS), 'Element type should be either "button", "icon" or "link."');
+    foreach ($this->getWebtoolsETransElements() as $data) {
+      $types_to_check = $type ? [$type] : ETransBlock::RENDER_OPTIONS;
+      foreach ($types_to_check as $type) {
+        if ($data->renderAs->$type ?? FALSE) {
+          throw new \RuntimeException("Webtools eTrans $type was unexpectedly found in the page.");
+        }
+      }
+    }
+  }
+
+  /**
+   * Returns an array of JSON data representing Webtools eTrans elements.
+   *
+   * @return object[]
+   *   The JSON data representing Webtools eTrans elements.
+   */
+  protected function getWebtoolsETransElements(): array {
+    $elements = [];
+
+    $xpath = '//div[contains(concat(" ", normalize-space(@class), " "), " block-oe-webtools-etrans ")]/script[@type="application/json"]';
+    /** @var \Behat\Mink\Element\NodeElement $element */
+    foreach ($this->getSession()->getPage()->findAll('xpath', $xpath) as $element) {
+      $data = json_decode($element->getHtml());
+
+      if (!empty($data) && !empty($data->service) && $data->service === 'etrans') {
+        $elements[] = $data;
+      }
+    }
+
+    return $elements;
+  }
+
+}
