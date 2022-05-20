408f7d4eab3399f5dcfc84a9a23fcbb45d9658f3
Imanol Eguskiza <imanoleguskizarojo@gmail.com>
2022-05-20 10:40:58 +0200

    EWPP-2243: Add wcloud support for webtools media sources.

--- modules/oe_webtools_media/src/Plugin/Validation/Constraint/WebtoolsMediaConstraint.php
+++ modules/oe_webtools_media/src/Plugin/Validation/Constraint/WebtoolsMediaConstraint.php
@@ -32,6 +32,27 @@ class WebtoolsMediaConstraint extends Constraint {
   public $blacklistMessage = 'This service is supported by a dedicated asset type or feature, please use that instead.';
 
   /**
+   * The incorrect wcloud url message.
+   *
+   * @var string
+   */
+  public $incorrectUrlMessage = 'The provided wcloud url is not valid.';
+
+  /**
+   * The incorrect wcloud url domain message.
+   *
+   * @var string
+   */
+  public $incorrectUrlDomainMessage = 'The wcloud url needs to be in the europa.eu domain.';
+
+  /**
+   * The incorrect wcloud url domain message.
+   *
+   * @var string
+   */
+  public $incorrectUrlContentMessage = 'Could not parse contents of the wcloud url.';
+
+  /**
    * The webtools widget type this constraint is checking for.
    *
    * @var string
--- modules/oe_webtools_media/src/Plugin/Validation/Constraint/WebtoolsMediaConstraintValidator.php
+++ modules/oe_webtools_media/src/Plugin/Validation/Constraint/WebtoolsMediaConstraintValidator.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 
 namespace Drupal\oe_webtools_media\Plugin\Validation\Constraint;
 
+use Drupal\Component\Utility\UrlHelper;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 use Drupal\Component\Serialization\Json;
@@ -16,6 +17,9 @@ class WebtoolsMediaConstraintValidator extends ConstraintValidator {
 
   /**
    * {@inheritdoc}
+   *
+   * @SuppressWarnings(PHPMD.CyclomaticComplexity)
+   * @SuppressWarnings(PHPMD.NPathComplexity)
    */
   public function validate($value, Constraint $constraint) {
     /** @var \Drupal\Core\Field\FieldItemListInterface $value */
@@ -45,6 +49,15 @@ class WebtoolsMediaConstraintValidator extends ConstraintValidator {
       return;
     }
 
+    // If it's a wcloud style service, assert its correct
+    // and try to fetch the actual snippet for validation.
+    if (!empty($snippet['utility']) && $snippet['utility'] == 'wcloud') {
+      $snippet = $this->parseWcloud($snippet, $constraint);
+      if (empty($snippet)) {
+        return;
+      }
+    }
+
     // Add violation in case incorrect services.
     $services = $widget_types[$constraint->widgetType]['services'] ?? [$widget_types[$constraint->widgetType]['service']];
     if (empty($snippet['service']) || (!empty($services) && !in_array($snippet['service'], $services))) {
@@ -56,6 +69,47 @@ class WebtoolsMediaConstraintValidator extends ConstraintValidator {
       // Add violation in case of blacklisted services.
       $this->context->addViolation($constraint->blacklistMessage);
     }
+  }
+
+  /**
+   * Attempts to parse and validate a Wcloud snippet.
+   *
+   * @param array $snippet
+   *   The snippet to ve parsed and validated.
+   * @param \Symfony\Component\Validator\Constraint $constraint
+   *   The constraint object.
+   *
+   * @return array
+   *   The parsed snippet if validation passed or an empty array otherwise.
+   */
+  protected function parseWcloud(array $snippet, Constraint $constraint): array {
+    // Assert if the url property is set and is valid.
+    if (empty($snippet['url']) || !UrlHelper::isValid($snippet['url'], TRUE)) {
+      $this->context->addViolation($constraint->incorrectUrlMessage);
+      return [];
+    }
+    // Assert that the url contains the europa.eu domain.
+    if (strpos(parse_url($snippet['url'], PHP_URL_HOST), 'europa.eu') === FALSE) {
+      $this->context->addViolation($constraint->incorrectUrlDomainMessage);
+      return [];
+    }
+
+    // Try to fetch and parse the response.
+    try {
+      $request = \Drupal::httpClient()->get($snippet['url']);
+      $wcloud_content = $request->getBody()->getContents();
+      $wcloud_snippet = Json::decode($wcloud_content);
+    }
+    catch (\Exception $exception) {
+      $this->context->addViolation($constraint->incorrectUrlContentMessage);
+      return [];
+    }
+    // Assert if parsing the response triggered any errors..
+    if (json_last_error() !== JSON_ERROR_NONE) {
+      $this->context->addViolation($constraint->incorrectUrlContentMessage);
+      return [];
+    }
+    return $wcloud_snippet;
   }
 
 }
