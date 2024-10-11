b4bdec9c1651d52f0c25fd107d5882ff1aedbb6c
drishu <aszilagyi@live.com>
2024-10-11 13:06:33 +0300

    OEL-3178: Use new config in maps formatter.

--- modules/oe_webtools_maps/src/Plugin/Field/FieldFormatter/WebtoolsMapFormatter.php
+++ modules/oe_webtools_maps/src/Plugin/Field/FieldFormatter/WebtoolsMapFormatter.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 
 namespace Drupal\oe_webtools_maps\Plugin\Field\FieldFormatter;
 
+use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\FormatterBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\oe_webtools_maps\Component\Render\JsonEncoded;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Displays a Geofield as a map using the Webtools Maps service.
@@ -21,6 +25,60 @@ use Drupal\oe_webtools_maps\Component\Render\JsonEncoded;
  * )
  */
 class WebtoolsMapFormatter extends FormatterBase {
+
+  /**
+   * The configuration factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * Constructs a WebtoolsMapFormatter object.
+   *
+   * @param string $plugin_id
+   *   The plugin_id for the formatter.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
+   *   The definition of the field to which the formatter is associated.
+   * @param array $settings
+   *   The formatter settings.
+   * @param string $label
+   *   The formatter label display setting.
+   * @param string $view_mode
+   *   The view mode.
+   * @param array $third_party_settings
+   *   Any third party settings.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $configFactory
+   *   The config factory.
+   */
+  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings, ConfigFactoryInterface $configFactory) {
+    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings);
+
+    $this->fieldDefinition = $field_definition;
+    $this->settings = $settings;
+    $this->label = $label;
+    $this->viewMode = $view_mode;
+    $this->thirdPartySettings = $third_party_settings;
+    $this->configFactory = $configFactory;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $plugin_id,
+      $plugin_definition,
+      $configuration['field_definition'],
+      $configuration['settings'],
+      $configuration['label'],
+      $configuration['view_mode'],
+      $configuration['third_party_settings'],
+      $container->get('config.factory')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -71,11 +129,12 @@ class WebtoolsMapFormatter extends FormatterBase {
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
     $element = [];
+    $config = $this->configFactory->get('oe_webtools_maps.settings');
 
     foreach ($items as $delta => $item) {
       $data_array = [
         'service' => 'map',
-        'version' => '2.0',
+        'version' => $config->get('map_version') ?? '2.0',
         'map' => [
           'zoom' => $this->getSetting('zoom_level'),
           'center' => [
@@ -129,6 +188,10 @@ class WebtoolsMapFormatter extends FormatterBase {
       $element['#attached'] = [
         'library' => ['oe_webtools/drupal.webtools-smartloader'],
       ];
+
+      $cache_metadata = new CacheableMetadata();
+      $cache_metadata->addCacheableDependency($config);
+      $cache_metadata->applyTo($element);
     }
 
     return $element;
