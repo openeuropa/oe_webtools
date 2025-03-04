71529f655efc98dfcfe61acc35438ca22223bacc
Victor Bourgade <victor.bourgade@gmail.com>
2025-03-04 10:28:17 +0000

    D8TEFD-1822: Fix drupal non ISO langcode.

--- modules/oe_webtools_etrans/src/Plugin/Block/UnifiedEtransBlock.php
+++ modules/oe_webtools_etrans/src/Plugin/Block/UnifiedEtransBlock.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Drupal\oe_webtools_etrans\Plugin\Block;
 
 use Drupal\Component\Serialization\Json;
+use Drupal\Component\Serialization\Yaml;
 use Drupal\Component\Utility\Html;
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Block\Attribute\Block;
@@ -101,10 +102,14 @@ class UnifiedEtransBlock extends BlockBase implements ContainerFactoryPluginInte
     $placeholder_id = Html::getUniqueId('etrans-widget');
     // On node route, we take the node original language,
     // which is not necessarily the default language of the website.
-    $translation_from = $this->getRouteEntityLangcode(TRUE) ?: $this->languageManager->getDefaultLanguage()->getId();
-    $translation_to_language = $this->languageManager->getCurrentLanguage();
+    $current_language = $this->languageManager->getCurrentLanguage();
+    $langcode_from = $this->getRouteEntityLangcode(TRUE) ?: $this->languageManager->getDefaultLanguage()->getId();
+    $langcode_to = $current_language->getId();
 
-    $json = $this->preparesWtEtransJson($translation_from, $placeholder_id);
+    $langcode_from = $this->mapLangcodeToIso($langcode_from);
+    $langcode_to = $this->mapLangcodeToIso($langcode_to);
+
+    $json = $this->preparesWtEtransJson($langcode_from, $placeholder_id);
 
     // Returns UEC webtool eTrans widget.
     $build['etrans_uec'] = [
@@ -120,7 +125,7 @@ class UnifiedEtransBlock extends BlockBase implements ContainerFactoryPluginInte
         ],
         'drupalSettings' => [
           'path' => [
-            'languageTo' => $translation_to_language->getId(),
+            'languageTo' => $langcode_to,
           ],
         ],
       ],
@@ -134,28 +139,28 @@ class UnifiedEtransBlock extends BlockBase implements ContainerFactoryPluginInte
     ];
 
     $message = $this->t("@language is available via eTranslation, the European Commission's machine translation service.", [
-      '@language' => $translation_to_language->getName(),
+      '@language' => $current_language->getName(),
     ], [
-      'langcode' => $translation_to_language->getId(),
+      'langcode' => $current_language->getId(),
     ]);
     $translate_link = [
       '#type' => 'link',
       '#url' => Url::fromRoute('<none>', [], ['attributes' => ['class' => ['oe-webtools-unified-etrans--translate']]]),
       '#title' => $this->t('Translate to @language', [
-        '@language' => $translation_to_language->getName(),
+        '@language' => $current_language->getName(),
       ], [
-        'langcode' => $translation_to_language->getId(),
+        'langcode' => $current_language->getId(),
       ]),
     ];
     $disclaimer_link = [
       '#type' => 'link',
-      '#url' => Url::fromUri('https://commission.europa.eu/languages-our-websites/use-machine-translation-europa_' . $translation_to_language->getId(), [
+      '#url' => Url::fromUri('https://commission.europa.eu/languages-our-websites/use-machine-translation-europa_' . $current_language->getId(), [
         'attributes' => [
           'class' => ['webtools-etrans--disclaimer'],
           'target' => '_blank',
         ],
       ]),
-      '#title' => $this->t('Important information about machine translation', [], ['langcode' => $translation_to_language->getId()]),
+      '#title' => $this->t('Important information about machine translation', [], ['langcode' => $current_language->getId()]),
     ];
     $build['translation_request'] = [
       '#theme' => 'block__unified_etrans_request',
@@ -282,6 +287,7 @@ class UnifiedEtransBlock extends BlockBase implements ContainerFactoryPluginInte
         'source' => $translation_from,
       ],
       'config' => [
+        'mode' => 'lc2023',
         'targets' => [
           'receiver' => "#$placeholder_id",
         ],
@@ -304,6 +310,31 @@ class UnifiedEtransBlock extends BlockBase implements ContainerFactoryPluginInte
     }
 
     return $json;
+  }
+
+  /**
+   * Attempt to map langcode to ISO 639-2.
+   *
+   * @param string $langcode
+   *   The drupal langcode.
+   *
+   * @return string
+   *   The ISO 639-2 langcode format.
+   */
+  protected function mapLangcodeToIso(string $langcode): string {
+    // In Drupal, langcode are often in ISO 639-2 format by default.
+    // But, some languages use browser formats like 'pt-pt' or 'ta-lk'.
+    // A mapping exists in the language module.
+    $mapping_file = file_get_contents(DRUPAL_ROOT . '/core/modules/language/config/install/language.mappings.yml');
+    $mapping = Yaml::decode($mapping_file ?: '');
+    $mapping = array_flip($mapping['map'] ?? []);
+    if (isset($mapping[$langcode])) {
+      return substr($mapping[$langcode], 0, 2);
+    }
+    // In some cases, users may add languages that are not standard.
+    // Due to this variability, the format of langcode is unpredictable.
+    // Therefore, we use only the first two characters as a best guess.
+    return substr($langcode, 0, 2);
   }
 
 }
